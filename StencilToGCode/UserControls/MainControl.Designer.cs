08dd0d524e529b69c0481e89dac16790001e848d
42jpryor <42jpryor@gmail.com>
2022-11-04 18:03:56 -0500

    Changed outline calculation. Added trackbar to for changing the limit at which a line is drawn. Scale image to printer size.

--- StencilToGCode/UserControls/MainControl.Designer.cs
+++ StencilToGCode/UserControls/MainControl.Designer.cs
@@ -33,9 +33,26 @@
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.btnConvert = new System.Windows.Forms.Button();
             this.btnSelectImage = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblColorLimit = new System.Windows.Forms.Label();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrinterWidth = new System.Windows.Forms.NumericUpDown();
+            this.txtPrinterHeight = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrinterLength = new System.Windows.Forms.NumericUpDown();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPrinterWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPrinterHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPrinterLength)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -70,14 +87,17 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel2.ColumnCount = 1;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Controls.Add(this.btnConvert, 0, 1);
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 2);
             this.tableLayoutPanel2.Controls.Add(this.btnSelectImage, 0, 0);
             this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 2;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel2.Size = new System.Drawing.Size(245, 412);
             this.tableLayoutPanel2.TabIndex = 1;
             // 
@@ -86,11 +106,11 @@
             this.btnConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnConvert.Location = new System.Drawing.Point(3, 209);
+            this.btnConvert.Location = new System.Drawing.Point(122, 3);
             this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(239, 200);
+            this.btnConvert.Size = new System.Drawing.Size(114, 42);
             this.btnConvert.TabIndex = 0;
-            this.btnConvert.Text = "Convert";
+            this.btnConvert.Text = "Convert To Outline";
             this.btnConvert.UseVisualStyleBackColor = true;
             this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
             // 
@@ -101,11 +121,180 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnSelectImage.Location = new System.Drawing.Point(3, 3);
             this.btnSelectImage.Name = "btnSelectImage";
-            this.btnSelectImage.Size = new System.Drawing.Size(239, 200);
+            this.btnSelectImage.Size = new System.Drawing.Size(239, 97);
             this.btnSelectImage.TabIndex = 1;
             this.btnSelectImage.Text = "Select Image";
             this.btnSelectImage.UseVisualStyleBackColor = true;
             this.btnSelectImage.Click += new System.EventHandler(this.btnSelectImage_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.SetColumnSpan(this.trackBar1, 2);
+            this.trackBar1.Location = new System.Drawing.Point(3, 51);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(233, 43);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.lblColorLimit, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnConvert, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.trackBar1, 0, 1);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 209);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(239, 97);
+            this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // lblColorLimit
+            // 
+            this.lblColorLimit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblColorLimit.AutoSize = true;
+            this.lblColorLimit.Location = new System.Drawing.Point(3, 17);
+            this.lblColorLimit.Name = "lblColorLimit";
+            this.lblColorLimit.Size = new System.Drawing.Size(113, 13);
+            this.lblColorLimit.TabIndex = 3;
+            this.lblColorLimit.Text = "0";
+            this.lblColorLimit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel4.ColumnCount = 4;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel4.Controls.Add(this.txtPrinterLength, 1, 2);
+            this.tableLayoutPanel4.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel4.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.label3, 0, 3);
+            this.tableLayoutPanel4.Controls.Add(this.txtPrinterHeight, 1, 3);
+            this.tableLayoutPanel4.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel4.Controls.Add(this.txtPrinterWidth, 1, 1);
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 106);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 4;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(239, 97);
+            this.tableLayoutPanel4.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.tableLayoutPanel4.SetColumnSpan(this.label1, 2);
+            this.label1.Location = new System.Drawing.Point(3, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Printer Size (mm)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Width:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Height:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtPrinterWidth
+            // 
+            this.txtPrinterWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPrinterWidth.Location = new System.Drawing.Point(62, 27);
+            this.txtPrinterWidth.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.txtPrinterWidth.Name = "txtPrinterWidth";
+            this.txtPrinterWidth.Size = new System.Drawing.Size(53, 20);
+            this.txtPrinterWidth.TabIndex = 3;
+            this.txtPrinterWidth.Value = new decimal(new int[] {
+            350,
+            0,
+            0,
+            0});
+            // 
+            // txtPrinterHeight
+            // 
+            this.txtPrinterHeight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPrinterHeight.Location = new System.Drawing.Point(62, 75);
+            this.txtPrinterHeight.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.txtPrinterHeight.Name = "txtPrinterHeight";
+            this.txtPrinterHeight.Size = new System.Drawing.Size(53, 20);
+            this.txtPrinterHeight.TabIndex = 4;
+            this.txtPrinterHeight.Value = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Length:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtPrinterLength
+            // 
+            this.txtPrinterLength.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPrinterLength.Location = new System.Drawing.Point(62, 51);
+            this.txtPrinterLength.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.txtPrinterLength.Name = "txtPrinterLength";
+            this.txtPrinterLength.Size = new System.Drawing.Size(53, 20);
+            this.txtPrinterLength.TabIndex = 6;
+            this.txtPrinterLength.Value = new decimal(new int[] {
+            350,
+            0,
+            0,
+            0});
             // 
             // MainControl
             // 
@@ -113,11 +302,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "MainControl";
-            this.Size = new System.Drawing.Size(861, 468);
+            this.Size = new System.Drawing.Size(965, 486);
             this.Load += new System.EventHandler(this.MainControl_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPrinterWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPrinterHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPrinterLength)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -129,5 +326,16 @@
         private System.Windows.Forms.Button btnConvert;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label lblColorLimit;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.NumericUpDown txtPrinterLength;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown txtPrinterHeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown txtPrinterWidth;
     }
 }
--- StencilToGCode/UserControls/MainControl.cs
+++ StencilToGCode/UserControls/MainControl.cs
@@ -24,14 +24,15 @@ namespace StencilToGCode.UserControls
 
         private void MainControl_Load(object sender, EventArgs e)
         {
-
+            trackBar1.Value = 220;
+            lblColorLimit.Text = trackBar1.Value.ToString();
         }
 
 
         private void btnSelectImage_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Filter = "Image Files (.bmp, .jpg, .png)|*.bmp;*.jpg;*.png";
+            openFileDialog1.Filter = "Image Files (.bmp, .jpg, .png)|*.bmp;*.jpg;*.png;";
             openFileDialog1.FilterIndex = 1;
 
             openFileDialog1.Multiselect = false;
@@ -49,7 +50,7 @@ namespace StencilToGCode.UserControls
                 {
                     // Read the first line from the file and write it the textbox.
                     btnSelectImage.Text = openFileDialog1.FileName;
-
+                    
                     pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
                 }
             }
@@ -61,60 +62,86 @@ namespace StencilToGCode.UserControls
             {
                 // Convert image to black and white
                 Bitmap bmp = new Bitmap(pictureBox1.Image);
-                Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
 
-                for (int x = 0; x < bmp.Width; x++)
+                int detailScale = 4;
+
+                // Scale image
+                int width = bmp.Width;
+                int height = bmp.Height;
+
+                if (width > height)
                 {
-                    for (int y = 0; y < bmp.Height; y++)
-                    {
-                        Color c = bmp.GetPixel(x, y);
-
-                        if (c.R < 200 && c.G < 200 && c.B < 200)
-                        {
-                            bmp2.SetPixel(x, y, Color.Black);
-                        }
-                        else
-                        {
-                            bmp2.SetPixel(x, y, Color.White);
-                        }
-                    }
+                    height = detailScale * (int)(txtPrinterLength.Value / width * height);
+                    width = detailScale * (int)txtPrinterWidth.Value;
+                }
+                else
+                {
+                    width = detailScale * (int)(txtPrinterWidth.Value / height * width);
+                    height = detailScale * (int)txtPrinterLength.Value;
                 }
 
-                // Create an outline of the image
-                Bitmap bmp3 = new Bitmap(bmp2.Width, bmp2.Height);
+                Bitmap bmp2 = new Bitmap(bmp, width, height);
 
-                // If the pixel is white, check the surrounding pixels, if the pixel is black set it to white
-                // If any of the surrounding pixels are black, set the pixel to black
+                // Convert to black and white
                 for (int x = 0; x < bmp2.Width; x++)
                 {
                     for (int y = 0; y < bmp2.Height; y++)
                     {
                         Color c = bmp2.GetPixel(x, y);
 
-                        if (c.R == 255 && c.G == 255 && c.B == 255)
+                        if (c.R + c.G + c.B > trackBar1.Value)
+                        {
+                            bmp2.SetPixel(x, y, Color.White);
+                        }
+                        else
+                        {
+                            bmp2.SetPixel(x, y, Color.Black);
+                        }
+                    }
+                }
+                
+
+                // Create an outline of the image
+                Bitmap bmp3 = new Bitmap(bmp2.Width, bmp2.Height);
+
+                // If the pixel is black, check the surrounding pixels
+                // If any of the surrounding pixels are white, set the pixel to black
+                for (int x = 0; x < bmp2.Width; x++)
+                {
+                    for (int y = 0; y < bmp2.Height; y++)
+                    {
+                        Color c = bmp2.GetPixel(x, y);
+
+                        if (c.R == 0 && c.G == 0 && c.B == 0)
                         {
                             // Check surrounding pixels
-                            bool foundBlack = false;
-
-                            for (int x2 = x - 1; x2 <= x + 1; x2++)
+                            if (x > 0 && y > 0 && x < bmp2.Width - 1 && y < bmp2.Height - 1)
                             {
-                                for (int y2 = y - 1; y2 <= y + 1; y2++)
+                                Color c2 = bmp2.GetPixel(x - 1, y);
+                                Color c3 = bmp2.GetPixel(x + 1, y);
+                                Color c4 = bmp2.GetPixel(x, y - 1);
+                                Color c5 = bmp2.GetPixel(x, y + 1);
+
+                                if (c2.R == 255 && c2.G == 255 && c2.B == 255)
                                 {
-                                    if (x2 >= 0 && x2 < bmp2.Width && y2 >= 0 && y2 < bmp2.Height)
-                                    {
-                                        Color c2 = bmp2.GetPixel(x2, y2);
-
-                                        if (c2.R == 0 && c2.G == 0 && c2.B == 0)
-                                        {
-                                            foundBlack = true;
-                                        }
-                                    }
+                                    bmp3.SetPixel(x, y, Color.Black);
                                 }
-                            }
-
-                            if (foundBlack)
-                            {
-                                bmp3.SetPixel(x, y, Color.Black);
+                                else if (c3.R == 255 && c3.G == 255 && c3.B == 255)
+                                {
+                                    bmp3.SetPixel(x, y, Color.Black);
+                                }
+                                else if (c4.R == 255 && c4.G == 255 && c4.B == 255)
+                                {
+                                    bmp3.SetPixel(x, y, Color.Black);
+                                }
+                                else if (c5.R == 255 && c5.G == 255 && c5.B == 255)
+                                {
+                                    bmp3.SetPixel(x, y, Color.Black);
+                                }
+                                else
+                                {
+                                    bmp3.SetPixel(x, y, Color.White);
+                                }
                             }
                             else
                             {
@@ -134,5 +161,10 @@ namespace StencilToGCode.UserControls
             }
         }
 
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            // Change label to show the value of the trackbar
+            lblColorLimit.Text = trackBar1.Value.ToString();
+        }
     }
 }
